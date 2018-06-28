e872d2dbe2b53b02d55989ddde2624dcc9740cf9
Ragacibi <ragacibi@gmail.com>
2018-06-28 15:45:46 +0530

    Now returns generator object

--- sorting.py
+++ sorting.py
@@ -7,8 +7,7 @@ def bub_sort(iterable, rev=False):
     '''This function does bubble 
 sort'''
     iterable = list(iterable)
-    repeat = sum(1 for elem in iterable) - 1
-    print()
+    repeat = len(iterable) - 1
     while repeat != 0:
         for _ in range(repeat):
             swap = True
@@ -16,12 +15,12 @@ sort'''
                 iterable[_],iterable[_+1] = iterable[_+1],iterable[_]
         
         repeat-=1
-#    iterable = map(iter,iterable)
-    yield map(iter,iterable)
+    if rev:
+        iterable = iterable[::-1]
+    for elm in iterable:
+        yield elm
 
 if __name__ == '__main__':
     IP = map(int, input("Enter integers: ").split())
     OP = bub_sort(IP)
     print(list(OP))
-
-exit(0)
